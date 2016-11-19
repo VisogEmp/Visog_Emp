b499bcb759ee86428fdea7afdcb49afbd6839453
visog <visogemp@gmail.com>
2016-11-19 15:36:53 +0530

    hai

---
+++ model/src/com/visog/Employee.java
@@ -0,0 +1,24 @@
+package com.visog;
+
+public class Employee {
+	private String Name;
+	
+	private String Name1;
+
+	public String getName() {
+		return Name;
+	}
+
+	public void setName(String name) {
+		Name = name;
+	}
+
+	public String getName1() {
+		return Name1;
+	}
+
+	public void setName1(String name1) {
+		Name1 = name1;
+	}
+
+}
---
+++ model/src/com/visog/Employee1.java
@@ -0,0 +1,10 @@
+package com.visog;
+
+public class Employee1 {
+	public static void main(String[] args) {
+		Employee e= new Employee();
+		System.out.println(e.getName1());
+		System.out.println(e.getName());
+	}
+
+}
